a21f0ce1bc1cb11fa1191cde7bcfba3548c32968
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-08-28 19:33:56 +0900

    refactor to general socket server

--- Chatting Server/app.js
+++ Chatting Server/app.js
@@ -2,34 +2,36 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-let room = [];
-let a = 0;
+// app.set('view engine', 'ejs');
+// app.set('views', './views');
+
+// app.get('/', (req, res) => {
+//     res.render('chat');
+//   });
+  
 
 io.on('connection',(socket)=>{
     socket.on('disconnect',()=>{
         console.log('user disconnected');
     });
 
-    socket.on('leaveRoom', (num, name) => {
-        socket.leave(room[num], ()=>{
-            console.log(name + 'leave a ' +room[num]);
-            io.to(room[num]).emit('leaveRoom', num, name);
-        })
+    socket.on('leaveRoom', (roomName, name) => {
+        io.to(roomName).emit('leaveRoom', roomName, name);
+
+        socket.leave(roomName)
     });
 
-    socket.on('joinRoom', (num, name) => {
-        socket.join(room[num], ()=>{
-            console.log(name + 'join a' + room[num]);
-            io.to(room[num]).emit('joinRoom',num,name);
-        })
+    socket.on('joinRoom', (roomName, name) => {
+        io.to(roomName).emit('joinRoom',roomName,name);
+
+        socket.join(roomName)
     });
 
-    socket.on('chat message', (num, name, msg)=>{
-        a = num;
-        io.to(room[a]).emit('chat message', name, msg);
+    socket.on('chat message', (roomName, name, msg)=>{
+        io.to(roomName).emit('chat message', name, msg);
     });
 
-    http.listem(3000, () => {
-        console.log('Connect at 3000');
-    })
+})
+http.listen(3334, () => {
+    console.log('Connect at 3334');
 })
