2e785eecb4de46d7d05b6491c9641d6fa8f3e3e8
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-08-28 18:05:47 +0900

    configuration

--- Streaming Server/thumbnail/thumbnail_generator.js
+++ Streaming Server/thumbnail/thumbnail_generator.js
@@ -1,12 +1,13 @@
 const spawn = require('child_process').spawn;
 const config = require('../config/config');
 const cmd = config.rtmp_server.trans.ffmpeg;
+const port = config.rtmp_server.http.port;
 
 const generateThumbnail = (streamKey) => {
 
     const args = [
         '-y',
-        '-i', 'http://127.0.0.1:8000/live/'+streamKey+'/index.m3u8',
+        '-i', `http://127.0.0.1:${port}/live/`+streamKey+'/index.m3u8',
         '-ss', '00:00:01',
         '-vframes', '1',
         '-vf', 'scale=-2:300',
